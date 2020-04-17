e8e7aa96abca83b27a85725949032fd6fe21067c
mitchellclay-asu <55418217+mitchellclay-asu@users.noreply.github.com>
2020-04-16 21:13:38 -0500

    Fixed fragment textView tags

--- app/src/main/java/com/example/regexcrossword/LetterChoiceFragment.java
+++ app/src/main/java/com/example/regexcrossword/LetterChoiceFragment.java
@@ -21,20 +21,28 @@ public class LetterChoiceFragment extends DialogFragment {
         String values = getArguments().getString("values");
         TextView tv00 = (TextView) rootView.findViewById(R.id.btnChoice_0_0);
         tv00.setText(values.substring(0,1));
+        tv00.setTag(values.substring(0,1));
         TextView tv10 = (TextView) rootView.findViewById(R.id.btnChoice_1_0);
         tv10.setText(values.substring(1,2));
+        tv10.setTag(values.substring(1,2));
         TextView tv20 = (TextView) rootView.findViewById(R.id.btnChoice_2_0);
         tv20.setText(values.substring(2,3));
+        tv20.setTag(values.substring(2,3));
         TextView tv01 = (TextView) rootView.findViewById(R.id.btnChoice_0_1);
         tv01.setText(values.substring(3,4));
+        tv01.setTag(values.substring(3,4));
         TextView tv21 = (TextView) rootView.findViewById(R.id.btnChoice_2_1);
         tv21.setText(values.substring(4,5));
+        tv21.setTag(values.substring(4,5));
         TextView tv02 = (TextView) rootView.findViewById(R.id.btnChoice_0_2);
         tv02.setText(values.substring(5,6));
+        tv02.setTag(values.substring(5,6));
         TextView tv12 = (TextView) rootView.findViewById(R.id.btnChoice_1_2);
         tv12.setText(values.substring(6,7));
+        tv12.setTag(values.substring(6,7));
         TextView tv22 = (TextView) rootView.findViewById(R.id.btnChoice_2_2);
         tv22.setText(values.substring(7,8));
+        tv22.setTag(values.substring(7,8));
         return rootView;
     }
 }
