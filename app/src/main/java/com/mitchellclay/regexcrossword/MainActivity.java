e38aef68fb6ac3fd7fc72391d681732ea35c4ea5
mitchellclay-asu <55418217+mitchellclay-asu@users.noreply.github.com>
2020-05-06 08:32:57 -0500

    updated layout, level selection

--- app/src/main/java/com/mitchellclay/regexcrossword/MainActivity.java
+++ app/src/main/java/com/mitchellclay/regexcrossword/MainActivity.java
@@ -2,9 +2,11 @@ package com.mitchellclay.regexcrossword;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +22,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loadTutorialSelectActivity(View view) {
-        Intent intent = new Intent(this, TutorialSelectActivity.class);
-        startActivity(intent);
+        //Intent intent = new Intent(this, TutorialSelectActivity.class);
+        //startActivity(intent);
+        Context context = getApplicationContext();
+        String text = "Coming soon!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
